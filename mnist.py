d6cad65f9785a4e51df74f4c99123c4dac42e293
Ubuntu <ubuntu@ip-172-31-44-193.eu-west-1.compute.internal>
2018-09-06 16:07:33 +0000

    working on MNIST (97.8% of acc) after 2 epochs

--- mnist.py
+++ mnist.py
@@ -30,7 +30,7 @@ val_loader = DataLoader(MNIST_data, batch_size=BATCH_SIZE, sampler=val_sampler, 
 model = CapsNet().to(device)
 # criterion = torch.nn.NLLLoss()
 criterion = MarginLoss(0.9, 0.1, 0.5)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 print('Number of parameters:', model.count_parameters())
 for epoch in range(NB_EPOCHS):
     print('Epoch {}/{}'.format(epoch + 1, NB_EPOCHS))
